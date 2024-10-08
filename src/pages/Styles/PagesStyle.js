c6efcf275d979b497b9d5e8e9b6d83b187be08e6
mateus-2002 <mateussilvaramos1454@gmail.com>
2024-10-08 00:20:09 -0300

    alteração na barra de pesquisas

--- src/pages/Styles/PagesStyle.js
+++ src/pages/Styles/PagesStyle.js
@@ -11,23 +11,35 @@ export const CharactersContainer = styled.div`
 
 export const InputFilter = styled.input`
     margin-top: 20px;
-    padding: 5px;
+    padding: 10px;
     border-radius: 4px;
     font-size: 15px;
-    border: solid 1px black;
+    border: solid 1px ${colors.pumpkin};
     width: 300px;
-`
+    background-color: rgba(255, 255, 255, 0.7);
+    color: black;
+
+    &::placeholder {
+        color: rgba(0, 0, 0, 0.5);
+        opacity: 1;
+    }
+
+    &:focus {
+        outline: none;
+        border-color: ${colors.darkSlateGray};
+    }
+`;
 
 export const LabelFilter = styled.label`
     color: black;
     margin-right: 5px;
     font-size: 18px;
     margin-bottom: 20px;
-`
+`;
 
 export const ButtonPagination = styled.button`
     color: white;
     margin: 30px 20px;
-    padding: 4 2 4 2 ;
+    padding: 4px 2px;
     background-color: ${colors.darkSlateGray};
-`
+`;
