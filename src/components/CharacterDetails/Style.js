3950b7f4fd260bfd63b3134835629900fcc1cbd1
BrenoJhonson <carlosbrenops01@gmail.com>
2024-10-08 00:35:21 -0300

    Alterações no Styles do CharacterDetails

--- src/components/CharacterDetails/Style.js
+++ src/components/CharacterDetails/Style.js
@@ -4,42 +4,49 @@ export const CharacterDetail = styled.div`
     width: 80vw;
     padding: 20px;
     display: flex;
-    justify-content: space-evenly;
-    align-items: center;
+    align-items: center; 
     background-image: url('https://images.stockcake.com/public/d/b/6/db6c3acb-c91d-4e64-9743-b1061ec91d9c_large/moonlit-swing-solitude-stockcake.jpg');
     background-size: cover; 
     background-position: center;
     background-repeat: no-repeat;
 
     & div {
-        width: 100%;
         display: flex;
-        flex-direction: column;
+        flex-direction: column; 
+        align-items: flex-start; 
         color: white; 
+        margin-left: 20px;
     }
-    & span {
-        display: flex; 
-        cursor: pointer;
-    }
+
     & img {
         width: 300px;
         height: 230px;
-    } 
+        margin-right: 20px; 
+    }
+
     & h3 {
         font-size: 25px;
         font-weight: bold;
+        margin: 10px 0;
     }
+    
     & label {
         font-size: 16px;
         font-weight: bold;
+        margin: 5px 0; 
+    }
+
+    & span {
+        font-size: 14px; 
+        margin: 5px 0; 
     }
 
     & .close-button {
-        color: red !important; /* Garante que a cor do 'X' seja vermelha */
-        font-size: 20px; /* Ajusta o tamanho da fonte do 'X' */
-        margin-bottom: 10px; /* Adiciona uma margem inferior para espaçamento */
+        color: red !important; 
+        font-size: 20px;
+        margin-bottom: 10px; 
         &:hover {
-            opacity: 0.8; /* Adiciona um efeito de hover */
+            opacity: 0.8; 
         }
     }
 `;
