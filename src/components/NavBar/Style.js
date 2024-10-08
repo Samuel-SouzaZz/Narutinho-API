5aefb4fb8b69454a10890e3eb3af66414513dc81
Samuel-SouzaZz <samucael8832@gmail.com>
2024-10-08 00:28:14 -0300

    Pequenas alterações

--- src/components/NavBar/Style.js
+++ src/components/NavBar/Style.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 import { colors } from "../../constans/colors";
 
-/* Container da NavBar, os itens são organizados em flex */
 export const NavBarContainer = styled.div`
     display: flex;
     justify-content: space-between; /* Deixa o gif e o logo separados */
@@ -11,21 +10,18 @@ export const NavBarContainer = styled.div`
     box-shadow: 0 2px 2px 2px ${colors.paleGoldenrod};
 `;
 
-/* Tamanho máximo para o logotipo */
 export const Logo = styled.img`
     max-height: 40px; 
 `;
 
-/* Wrapper para centralizar o logo */
 export const LogoWrapper = styled.div`
     display: flex;
-    flex-grow: 1; /* Faz o logo ocupar o espaço central */
-    justify-content: center; /* Centraliza o logo no espaço disponível */
+    flex-grow: 1; 
+    justify-content: center; 
 `;
 
-/* Gif no início, sem alteração de estilo */
 export const NarutoGif = styled.img`
     width: 100px; 
     height: auto; 
-    margin-right: 10px; /* Um pouco de espaço entre o gif e o logo */
+    margin-right: 10px;
 `;
