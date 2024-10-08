80eb917eaa479875216d888adf6bed5e1581d34c
Samuel-SouzaZz <samucael8832@gmail.com>
2024-10-08 00:26:26 -0300

    alteração Style NavBar.js

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,15 +1,17 @@
 import React from "react";
 import logo from "../../_assets/naruto-logo.png";
-import { NavBarContainer, Logo, NarutoGif } from './Style';
+import { NavBarContainer, Logo, NarutoGif, LogoWrapper } from './Style';
 
 export const NavBar = () => {
   return (
     <NavBarContainer>
-      <Logo src={logo} alt="Logo Naruto" />
       <NarutoGif 
         src="https://media.tenor.com/pf19SFYHd1IAAAAi/naruto-uzumaki.gif" 
         alt="Naruto" 
       />
+      <LogoWrapper>
+        <Logo src={logo} alt="Logo Naruto" />
+      </LogoWrapper>
     </NavBarContainer>
   );
 };
--- src/components/NavBar/Style.js
+++ src/components/NavBar/Style.js
@@ -1,20 +1,31 @@
 import styled from "styled-components";
 import { colors } from "../../constans/colors";
 
+/* Container da NavBar, os itens são organizados em flex */
 export const NavBarContainer = styled.div`
     display: flex;
-    align-items: center; /* Alinha os itens verticalmente ao centro */
+    justify-content: space-between; /* Deixa o gif e o logo separados */
+    align-items: center; /* Alinha verticalmente ao centro */
     background-color: ${colors.pumpkin};
     padding: 10px 20px;
     box-shadow: 0 2px 2px 2px ${colors.paleGoldenrod};
 `;
 
+/* Tamanho máximo para o logotipo */
 export const Logo = styled.img`
-    max-height: 40px; /* Tamanho máximo para o logotipo */
+    max-height: 40px; 
 `;
 
+/* Wrapper para centralizar o logo */
+export const LogoWrapper = styled.div`
+    display: flex;
+    flex-grow: 1; /* Faz o logo ocupar o espaço central */
+    justify-content: center; /* Centraliza o logo no espaço disponível */
+`;
+
+/* Gif no início, sem alteração de estilo */
 export const NarutoGif = styled.img`
-    width: 100px; /* ajuste o tamanho conforme necessário */
-    height: auto; /* mantém a proporção */
-    margin-left: 10px; /* espaço à esquerda do gif */
+    width: 100px; 
+    height: auto; 
+    margin-right: 10px; /* Um pouco de espaço entre o gif e o logo */
 `;
