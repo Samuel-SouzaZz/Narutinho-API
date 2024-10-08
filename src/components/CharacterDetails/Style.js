a021d878f40be458fab4f03cf941314671ba17d0
BrenoJhonson <carlosbrenops01@gmail.com>
2024-10-08 00:19:02 -0300

    Alterações no button

--- src/components/CharacterDetails/Style.js
+++ src/components/CharacterDetails/Style.js
@@ -7,19 +7,19 @@ export const CharacterDetail = styled.div`
     justify-content: space-evenly;
     align-items: center;
     background-image: url('https://images.stockcake.com/public/d/b/6/db6c3acb-c91d-4e64-9743-b1061ec91d9c_large/moonlit-swing-solitude-stockcake.jpg');
-    background-size: cover; /* Ajusta a imagem para cobrir o elemento */
-    background-position: center; /* Centraliza a imagem */
-    background-repeat: no-repeat; /* Evita a repetição da imagem */
+    background-size: cover; 
+    background-position: center;
+    background-repeat: no-repeat;
 
     & div {
         width: 100%;
         display: flex;
         flex-direction: column;
-        color: white; /* Muda a cor do texto para branco para melhor contraste */
+        color: white; 
     }
     & span {
         display: flex; 
-        cursor: pointer; /* Altera o cursor para indicar que é clicável */
+        cursor: pointer;
     }
     & img {
         width: 300px;
@@ -34,10 +34,12 @@ export const CharacterDetail = styled.div`
         font-weight: bold;
     }
 
-    /* Estilo específico para o 'X' */
     & .close-button {
         color: red; /* Define a cor do 'X' como vermelho */
         font-size: 20px; /* Ajusta o tamanho da fonte do 'X' */
         margin-bottom: 10px; /* Adiciona uma margem inferior para espaçamento */
+        &:hover {
+            opacity: 0.8; /* Adiciona um efeito de hover */
+        }
     }
 `;
